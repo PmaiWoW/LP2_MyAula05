3251e2fadbe2bd72033aea4e2af643ac4c9d73b0
PmaiWoW <PmaiWoW@Gmail.com>
2019-10-23 15:40:35 +0100

    Implementar vários blocos catch, com mensagens personalizadas para diferentes exceptions possíveis

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -11,9 +11,16 @@ namespace ExceptionTest
                 Console.WriteLine("Insere um número inteiro: ");
                 int i = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"Número inserido: {i}");
+            } catch (FormatException e)
+            {
+                Console.WriteLine("\nNão foi inserido um número inteiro.");
+            } catch (OverflowException e)
+            {
+                Console.WriteLine("\nFoi inserido um número inteiro " +
+                    "demasiado grande ou pequeno.");
             } catch (Exception e)
             {
-                Console.WriteLine($"Ocorreu o seguinte erro: {e.Message}");
+                Console.WriteLine($"\nOcorreu o seguinte erro: {e.Message}");
             }
         }
     }
