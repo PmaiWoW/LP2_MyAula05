61c14e0b0085f88e1b1640787bfa56674e870013
PmaiWoW <PmaiWoW@Gmail.com>
2019-10-23 15:11:49 +0100

    Exercício de teste de exceção para aula1

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insere um número inteiro: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Número inserido: {i}");
+        }
+    }
+}
